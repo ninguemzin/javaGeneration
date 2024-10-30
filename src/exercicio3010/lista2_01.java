045b107f4f2bc85d7eeace2038c44dfc88d27b69
ninguemzin <alm1427@hotmail.com>
2024-10-30 15:18:16 -0300

    lista2_01

--- src/exercicio3010/lista2_01.java
+++ src/exercicio3010/lista2_01.java
@@ -1,10 +1,31 @@
 package exercicio3010;
 
+import java.util.Scanner;
+
 public class lista2_01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int idade = 0, maior = 0, menor = 0;
+		while (true) {
+			System.out.println("Digite uma idade (numero negativo para sair): ");
+			idade = scanner.nextInt();
+			
+			if(idade < 0) {
+				break;
+			}
+			if (idade < 21) {
+				menor++;
+			}
+			if (idade > 50) {
+				maior++;
+			}
+		}
+		
+		System.out.println("pessoas menores que 21: " + menor);
+		System.out.println("pessoas maiores que 60: " + maior);
+		
+		scanner.close();
 	}
 
 }
